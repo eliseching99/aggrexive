5d98a58bfc2306b03f95aefb554a578c33587b18
eleezy99 <eliseching1999@gmail.com>
2020-09-26 18:23:31 +0800

    added all

---
+++ assets/js/modal.js
@@ -0,0 +1,11 @@
+$("#modal1").click(function(){
+    $("#modal1show").modal("show");
+});
+
+$("#modal2").click(function(){
+    $("#modal2show").modal("show");
+});
+
+$("#modal3").click(function(){
+    $("#modal3show").modal("show");
+});
